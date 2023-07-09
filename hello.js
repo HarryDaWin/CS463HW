73c4694008b458f8168632479687abfe8c8e0acb
Harrison Huynh <harrisonhuynh999@gmail.com>
2023-07-09 10:23:18 -0700

    akdjlf

---
+++ hello.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', loadPage);
+
+function loadPage(event) {
+  console.log('The page has now loaded.');
+  console.log('My name is Harrison Huynh, I am a computer science student attending Portland State. I work at Thai Bloom while I go to school. I enjoy gaming, snowboarding, and pickleball.')
+}
