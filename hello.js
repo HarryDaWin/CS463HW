12a060915d33c722056f8bcf8b905a6a50eecc29
Harrison Huynh <harrisonhuynh999@gmail.com>
2023-07-10 11:14:57 -0700

    prettier

--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
-  console.log('My name is Harrison Huynh, I am a computer science student attending Portland State. I work at Thai Bloom while I go to school. I enjoy gaming, snowboarding, and pickleball.')
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Harrison Huynh, I am a computer science student attending Portland State. I work at Thai Bloom while I go to school. I enjoy gaming, snowboarding, and pickleball."
+  );
 }
